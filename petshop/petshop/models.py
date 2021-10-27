0e1b3821c06fb9a4833c7ef467e1d0addf851aaa
Leonardo Fernandes <leofernandes2169@gmail.com>
2021-10-27 00:25:20 -0300

    criação de model

--- petshop/petshop/models.py
+++ petshop/petshop/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Clientes(models.Model):
+    nome = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    cpf = models.CharField(
+        max_length=14,
+        null=False,
+        blank=False
+    )
+
+    telefone = models.CharField(
+        max_length=20,
+        null=False,
+        blank=False
+    )
+
+    email = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False
+    )
+
+    endereco = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False
+    )
+
