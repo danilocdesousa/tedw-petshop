8fbbbeb0c5be360ed918c7d17d9065abc08fead8
Danilo de Sousa <45634291+danilocdesousa@users.noreply.github.com>
2021-10-31 20:19:02 -0300

    Update urls.py
    Configuração de rotas para a UpdateView.

--- petshop/website/urls.py
+++ petshop/website/urls.py
@@ -1,6 +1,6 @@
 # Importação das funções definidas no arquivo views.py.
 from django.urls import path
-from website.views import IndexTemplateView, ClientesListView
+from website.views import IndexTemplateView, ClientesListView, ClientesUpdateView
 
 app_name = 'website'
 
@@ -11,4 +11,8 @@ urlpatterns = [
     
     # GET /clientes
     path('clientes/', ClientesListView.as_view(), name="lista_clientes"),
+    
+    # Para atualizar o objeto, devemos buscá-lo via 'id' ou 'slug'. Também pode ser atualizado através do método get_object().
+    # GET/POST /funcionario/{pk do objeto}
+    path('clientes/<pk>', ClientesUpdateView.as_view(), name = "atualiza_clientes"),
 ]
