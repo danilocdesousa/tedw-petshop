f2135ab019cb62d5325057a7094b919b594b2966
Danilo de Sousa <45634291+danilocdesousa@users.noreply.github.com>
2021-10-31 20:10:10 -0300

    Update views.py
    Utilizando a UpdateView para a atualização de clientes.

--- petshop/website/views.py
+++ petshop/website/views.py
@@ -1,5 +1,7 @@
 from registration.models import Clientes
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, UpdateView
+from django.urls import reverse_lazy
+
 
 # Utilizando a TemplateView para rederização da página.
 class IndexTemplateView(TemplateView):
@@ -10,3 +12,11 @@ class ClientesListView(ListView):
     template_name = "website/lista.html"
     model = Clientes
     context_object_name = 'clientes'
+    
+# Utilizando a UpdateView para a atualização de clientes.
+class ClientesUpdateView(UpdateView):
+    template_name = "website/atualiza.html"
+    model = Clientes
+    fields = '__all__' # fields = ['nome', 'cpf', 'telefone', 'email', 'endereco']
+    context_object_name = 'clientes'
+    success_url = reverse_lazy("website:lista_clientes")
