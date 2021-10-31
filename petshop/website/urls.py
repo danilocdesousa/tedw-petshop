ad21f3fa997cdab26da1918cad1f146a7b455d8b
Danilo de Sousa <45634291+danilocdesousa@users.noreply.github.com>
2021-10-31 20:26:00 -0300

    Update urls.py
    Configuração do id a ser buscado no URLConf para a DeleteView.

--- petshop/website/urls.py
+++ petshop/website/urls.py
@@ -1,6 +1,6 @@
 # Importação das funções definidas no arquivo views.py.
 from django.urls import path
-from website.views import IndexTemplateView, ClientesListView, ClientesUpdateView
+from website.views import IndexTemplateView, ClientesListView, ClientesUpdateView, ClientesDeleteView
 
 app_name = 'website'
 
@@ -13,6 +13,10 @@ urlpatterns = [
     path('clientes/', ClientesListView.as_view(), name="lista_clientes"),
     
     # Para atualizar o objeto, devemos buscá-lo via 'id' ou 'slug'. Também pode ser atualizado através do método get_object().
-    # GET/POST /funcionario/{pk do objeto}
+    # GET/POST /cliente/{pk do objeto}
     path('clientes/<pk>', ClientesUpdateView.as_view(), name = "atualiza_clientes"),
+    
+    # Para deletar o objeto, o procedimento é parecido com o de atualização.
+    # GET/POST /cliente/excluir/{pk do objeto}
+    path('clientes/excluir/<pk>',ClientesDeleteView.as_view(), name="deleta_clientes"),
 ]
