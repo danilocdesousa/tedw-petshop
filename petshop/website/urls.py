70f55acc098afdd7b2cb5f7c65382a3f55bd1017
Danilo de Sousa <45634291+danilocdesousa@users.noreply.github.com>
2021-10-31 19:31:57 -0300

    Configuração de rotas IndexTemplateView
    Atualização do urls.py do app website.

--- petshop/website/urls.py
+++ petshop/website/urls.py
@@ -1,1 +1,11 @@
+# Importação das funções definidas no arquivo views.py.
+from django.urls import path
+from website.views import IndexTemplateView
+
 app_name = 'website'
+
+# A urlpatterns contém a lista de roteamentos de URLs.
+urlpatterns = [
+    # GET /
+    path('', IndexTemplateView.as_view(), name="index"),
+]
