b70de6b9e15787cf72fdcd134b2ac92bbdb68060
Danilo de Sousa <cdanilodesousa@gmail.com>
2021-11-07 14:52:35 -0300

    Update base.html - footer

--- petshop/website/static/website/js/scripts.js
+++ petshop/website/static/website/js/scripts.js
@@ -2,7 +2,8 @@
 var bodyColor = document.querySelector('body.bg-secondary');
 var linkColor = document.querySelector('a#interruptor');
 linkColor.addEventListener('click', function () {
-    var aberto = bodyColor.classList.contains('bg-light');
+    var dark = bodyColor.classList.contains('bg-light');
     bodyColor.classList.toggle('bg-light');
+    this.innerHTML = dark ? 'night' : 'day';
 });
 */
