7a682badecf683a8a73e728f7c55d6b583d5b0a9
Danilo de Sousa <cdanilodesousa@gmail.com>
2021-11-09 03:53:04 -0300

    DeleteView do app services

--- petshop/services/urls.py
+++ petshop/services/urls.py
@@ -1,6 +1,6 @@
 # Importação das funções definidas no arquivo views.py.
 from django.urls import path
-from services.views import ServicesCreateView, ServicesListView, ServicesUpdateView
+from services.views import ServicesCreateView, ServicesListView, ServicesUpdateView, ServicesDeleteView
 
 app_name = 'services'
 
@@ -13,6 +13,10 @@ urlpatterns = [
     path('listar/', ServicesListView.as_view(), name="lista_services"),
 
     # Para atualizar o objeto, devemos buscá-lo via 'id' ou 'slug'. Também pode ser atualizado através do método get_object().
-    # GET/POST /services/{pk do objeto}
-    path('<pk>', ServicesUpdateView.as_view(), name = "atualiza_services"),
+    # GET/POST /services (services está definido em url.py petshop)/{pk do objeto}
+    path('atualizar/<pk>', ServicesUpdateView.as_view(), name="atualiza_services"),
+
+    # Para deletar o objeto, o procedimento é parecido com o de atualização.
+    # GET/POST /services (services está definido em url.py petshop)/excluir/{pk do objeto}
+    path('excluir/<pk>', ServicesDeleteView.as_view(), name="deleta_services"),
 ]
--- petshop/services/views.py
+++ petshop/services/views.py
@@ -1,11 +1,10 @@
 from petshop.models import Services
-from django.views.generic import CreateView, ListView, UpdateView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from services.forms import InsereServicesForm
 
+
 # Utilizando a CreateView para adicionar um novo serviço.
-
-
 class ServicesCreateView(CreateView):
     template_name = "services/cria.html"
     model = Services
@@ -13,9 +12,8 @@ class ServicesCreateView(CreateView):
     # O método reverse_lazy() vai traduzir a View em URL. Após adicionar um serviço, haverá um redirecionamento para a página de listagem atualizada.
     success_url = reverse_lazy("services:lista_services")
 
+
 # Utilizando a ListView para a listagem de serviços.
-
-
 class ServicesListView(ListView):
     template_name = "services/lista.html"
     model = Services
@@ -27,5 +25,13 @@ class ServicesUpdateView(UpdateView):
     template_name = "services/atualiza.html"
     model = Services
     fields = '__all__'  # fields = ['nome', 'preco']
+    context_object_name = 'services'
+    success_url = reverse_lazy("services:lista_services")
+
+
+# Utilizando a DeleteView para deletar um serviço.
+class ServicesDeleteView(DeleteView):
+    template_name = "services/exclui.html"
+    model = Services()
     context_object_name = 'services'
     success_url = reverse_lazy("services:lista_services")
