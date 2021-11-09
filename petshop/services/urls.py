4e7ce02684a3455a018c36c044115d07dbfa47e6
Danilo de Sousa <cdanilodesousa@gmail.com>
2021-11-09 03:37:12 -0300

    ListView do app services

--- petshop/services/urls.py
+++ petshop/services/urls.py
@@ -1,12 +1,14 @@
 # Importação das funções definidas no arquivo views.py.
 from django.urls import path
-from services.views import ServicesCreateView
+from services.views import ServicesCreateView, ServicesListView
 
 app_name = 'services'
 
 # A urlpatterns contém a lista de roteamentos de URLs.
 urlpatterns = [
     # GET /cadastrar
-    path('cadastrar/', ServicesCreateView.as_view(),
-         name="cadastra_services"),
+    path('cadastrar/', ServicesCreateView.as_view(), name="cadastra_services"),
+
+    # GET /listar
+    path('listar/', ServicesListView.as_view(), name="lista_services"),
 ]
--- petshop/services/views.py
+++ petshop/services/views.py
@@ -1,5 +1,5 @@
 from petshop.models import Services
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
 from services.forms import InsereServicesForm
 
@@ -12,3 +12,11 @@ class ServicesCreateView(CreateView):
     form_class = InsereServicesForm
     # O método reverse_lazy() vai traduzir a View em URL. Após adicionar um serviço, haverá um redirecionamento para a página de listagem atualizada.
     success_url = reverse_lazy("services:lista_services")
+
+# Utilizando a ListView para a listagem de serviços.
+
+
+class ServicesListView(ListView):
+    template_name = "services/lista.html"
+    model = Services
+    context_object_name = 'services'
